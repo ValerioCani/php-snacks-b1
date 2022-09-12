fbb28ca0effb6de44c38a201946a746c906789e1
Valerio <canivalerio93@gmail.com>
2022-09-12 16:27:26 +0200

    fatto terzo snack

---
+++ snack-7/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $classe = [
+        $alunno1 = [
+            $nome= "Luca",
+            $cognome= "Rossi",
+            $voti= [
+                7,
+                8,
+                5,
+                7,
+                6
+            ], 
+        ],
+        $alunno2 = [
+            $nome= "Gloria",
+            $cognome= "Bianchi",
+            $voti= [
+                5,
+                3,
+                6,
+                4
+            ], 
+        ],
+        $alunno3 = [
+            $nome= "Lucia",
+            $cognome= "Verdi",
+            $voti= [
+                8,
+                10,
+                9,
+                7,
+                10,
+                9,
+                8
+            ], 
+        ],
+    ]
+    ?>
+    <ul>
+        <?php
+            for($i=0; $i<count($classe); $i++){
+                $alunno = $classe[$i];
+                echo "<p>nome: ". $alunno[0]. "/ cognome: ". $alunno[1]. "</p>";
+                $mediavoti = ceil(array_sum($alunno[2]) / count($alunno[2]) * 10) / 10;
+                echo "<p>Voto in Pagella: ". $mediavoti ."</p>";
+            }
+        ?>
+    </ul>
+</body>
+</html>
